0c77050c6b4d7a577b2415d9434040005efc7190
prathamesh <prathamesh.khadkban@gmail.com>
2022-03-08 23:50:00 +0530

    make it attractive by integrate in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Colorize from "./colorize/Colorize";
+import { useState } from "react";
 
 function App() {
+  const getRandomColor = () =>
+    `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+  const [color, setColor] = useState(getRandomColor);
+
+  const changeColorHandler = () => setColor(getRandomColor);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header style={{ backgroundColor: color }} className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Colorize></Colorize>
+        <button onClick={changeColorHandler}>Change Color</button>
       </header>
     </div>
   );
